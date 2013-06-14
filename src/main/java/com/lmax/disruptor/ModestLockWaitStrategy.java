48135a3a60ab5bec7fb2ac14e443699865f57904
qinxian <fyaoxy@gmail.com>
2013-06-14 09:53:53 +0800

    add faster modest-lock WaitStrategy implementation

---
+++ src/main/java/com/lmax/disruptor/ModestLockWaitStrategy.java
@@ -0,0 +1,71 @@
+/*
+ * Copyright 2011 LMAX Ltd.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.lmax.disruptor;
+
+
+/**
+ * Yielding strategy that uses a Thread.yield() for {@link com.lmax.disruptor.EventProcessor}s waiting on a barrier
+ * after an initially spinning.
+ *
+ * This strategy is a good compromise between performance and CPU resource without incurring significant latency spikes.
+ */
+public final class ModestLockWaitStrategy implements WaitStrategy
+{
+    private static final int SPIN_TRIES = 100;
+
+    @Override
+    public long waitFor(final long sequence, Sequence cursor, final Sequence dependentSequence, final SequenceBarrier barrier)
+        throws AlertException, InterruptedException
+    {
+        long availableSequence;
+        int counter = SPIN_TRIES;
+
+        while ((availableSequence = dependentSequence.get()) < sequence)
+        {
+            counter = applyWaitMethod(barrier, counter);
+        }
+
+        return availableSequence;
+    }
+
+    @Override
+    public void signalAllWhenBlocking()
+    {
+    }
+
+    /**
+     * This way faster than {@link YieldingWaitStrategy}.
+     *
+     * Seems fair for threads, So I call it as modest-lock, just like the modest.
+     *
+     * the pure yield mode by counter, should be "strong modest", not the true modest.
+     *
+     * @author qinxian
+     *
+     * TODO: refactor the lockless {@link WaitStrategy} and subclasses
+     */
+    private int applyWaitMethod(final SequenceBarrier barrier, int counter)
+        throws AlertException
+    {
+        barrier.checkAlert();
+        if ((counter&1)==1)
+        {
+            Thread.yield();
+        }
+
+        return --counter;
+    }
+}
